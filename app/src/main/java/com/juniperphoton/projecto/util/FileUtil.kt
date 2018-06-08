077232e3e524f61b000bfbe255bba353ac2e6cf3
JuniperPhoton <dengweichao@hotmail.com>
2018-06-08 15:21:42 +0800

    Rename project o to screen decor

--- app/src/main/java/com/juniperphoton/projecto/util/FileUtil.kt
+++ app/src/main/java/com/juniperphoton/projecto/util/FileUtil.kt
@@ -3,6 +3,7 @@ package com.juniperphoton.projecto.util
 import android.content.Context
 import android.os.Environment
 import android.util.Log
+import com.juniperphoton.projecto.R
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,22 +20,24 @@ object FileUtil {
 
         // Create a media file name
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        return File(mediaStorageDir + File.separator + "IMG_" + timeStamp + ".png")
+        return File(mediaStorageDir + File.separator + "DECOR_" + timeStamp + ".png")
     }
 
     private fun getMediaStorageDir(context: Context): String? {
+        val folderName = context.resources.getString(R.string.app_name)
+
         val mediaStorageDir: File
         mediaStorageDir = if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
             val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) ?: return ""
-            File(path, "ProjectO")
+            File(path, folderName)
         } else {
             val extStorageDirectory = context.filesDir.absolutePath
-            File(extStorageDirectory, "ProjectO")
+            File(extStorageDirectory, folderName)
         }
 
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.i("FileUtil", "Error failed to create directory")
+                Log.i(TAG, "Error failed to create directory")
                 return null
             }
         }
